ac22dd9d99aeb61390b397efddf5a89ff7516a86
Joel Gaspar <joel.e.gaspar@gmail.com>
2021-01-19 18:04:40 -0700

    saving data from can you run it

--- src/requirements.py
+++ src/requirements.py
@@ -3,49 +3,89 @@
 from sqlalchemy import Column, String, Integer, Text
 from base import Base
 
-class LowReqSteam(Base):
-    def __init__(self ,Game_Name, Descr, OS, Processor, Ram, Graphics, DirectX,size,Notes,table_name):
-        self.tablename = tablename
-        self.Game_Name = Game_Name
-        self.Descr = Descr
-        self.OS = OS
-        self.Processor = Processor
-        self.Ram = Ram
-        self.Graphics = Graphics
-        self.DirectX = DirectX
-        self.size = size
-        self.Notes = Notes
-        table()
+# class LowReqSteam(Base):
+#     __tablename__ = 'low_req_steam'
+#     id = Column(Integer(), primary_key=True, autoincrement = True)
+#     Game_Name = Column(String(255))
+#     Descr = Column(String(255))
+#     OS = Column(String(255))
+#     Processor = Column(String(255))
+#     Ram = Column(String(255))
+#     Graphics = Column(String(255))
+#     DirectX = Column(String(255))
+#     size = Column(String(255))
+#     Notes = Column(String(255))
+
+#     def __init__(self ,Game_Name, Descr, OS, Processor, Ram, Graphics, DirectX,size,Notes,table_name):
+#         self.Game_Name = Game_Name
+#         self.Descr = Descr
+#         self.OS = OS
+#         self.Processor = Processor
+#         self.Ram = Ram
+#         self.Graphics = Graphics
+#         self.DirectX = DirectX
+#         self.size = size
+#         self.Notes = Notes
         
-    def table(self):
-        __tablename__ = self.tablename
-        id = Column(Integer(), primary_key=True, autoincrement = True)
-        Game_Name = Column(String(255))
-        Descr = Column(String(255))
-        OS = Column(String(255))
-        Processor = Column(String(255))
-        Ram = Column(String(255))
-        Graphics = Column(String(255))
-        DirectX = Column(String(255))
-        size = Column(String(255))
-        Notes = Column(String(255))
+        
+
     
 
-class RecReqSteam(Base):
-    def __init__(self ,Game_Name, Descr, OS, Processor, Ram, Graphics, DirectX,size,Notes):
+# class RecReqSteam(Base):
+#     def __init__(self ,Game_Name, Descr, OS, Processor, Ram, Graphics, DirectX,size,Notes):
        
-        self.Game_Name = Game_Name
-        self.Descr = Descr
-        self.OS = OS
-        self.Processor = Processor
-        self.Ram = Ram
-        self.Graphics = Graphics
-        self.DirectX = DirectX
-        self.size = size
-        self.Notes = Notes
+#         self.Game_Name = Game_Name
+#         self.Descr = Descr
+#         self.OS = OS
+#         self.Processor = Processor
+#         self.Ram = Ram
+#         self.Graphics = Graphics
+#         self.DirectX = DirectX
+#         self.size = size
+#         self.Notes = Notes
         
         
-    __tablename__ = 'rec_req_steam'
+#     __tablename__ = 'rec_req_steam'
+#     id = Column(Integer(), primary_key=True, autoincrement = True)
+#     Game_Name = Column(String(255))
+#     Descr = Column(String(255))
+#     OS = Column(String(255))
+#     Processor = Column(String(255))
+#     Ram = Column(String(255))
+#     Graphics = Column(String(255))
+#     DirectX = Column(String(255))
+#     size = Column(String(255))
+#     Notes = Column(String(255))
+    
+# class LowReqPCGBM(Base):
+#     def __init__(self ,Game_Name, Descr, OS, Processor, Ram, Graphics, DirectX,size,Notes):
+       
+#         self.Game_Name = Game_Name
+#         self.Descr = Descr
+#         self.OS = OS
+#         self.Processor = Processor
+#         self.Ram = Ram
+#         self.Graphics = Graphics
+#         self.DirectX = DirectX
+#         self.size = size
+#         self.Notes = Notes
+        
+        
+#     __tablename__ = 'low_req_pcbenchmark'
+#     id = Column(Integer(), primary_key=True, autoincrement = True)
+#     Game_Name = Column(String(255))
+#     Descr = Column(String(255))
+#     OS = Column(String(255))
+#     Processor = Column(String(255))
+#     Ram = Column(String(255))
+#     Graphics = Column(String(255))
+#     DirectX = Column(String(255))
+#     size = Column(String(255))
+#     Notes = Column(String(255))
+
+
+class LowReqCanYouRunIt(Base):
+    __tablename__ = 'low_req_canyourunit'
     id = Column(Integer(), primary_key=True, autoincrement = True)
     Game_Name = Column(String(255))
     Descr = Column(String(255))
@@ -57,9 +97,7 @@ class RecReqSteam(Base):
     size = Column(String(255))
     Notes = Column(String(255))
     
-class LowReqPCGBM(Base):
     def __init__(self ,Game_Name, Descr, OS, Processor, Ram, Graphics, DirectX,size,Notes):
-       
         self.Game_Name = Game_Name
         self.Descr = Descr
         self.OS = OS
@@ -69,16 +107,3 @@ class LowReqPCGBM(Base):
         self.DirectX = DirectX
         self.size = size
         self.Notes = Notes
-        
-        
-    __tablename__ = 'low_req_pcbenchmark'
-    id = Column(Integer(), primary_key=True, autoincrement = True)
-    Game_Name = Column(String(255))
-    Descr = Column(String(255))
-    OS = Column(String(255))
-    Processor = Column(String(255))
-    Ram = Column(String(255))
-    Graphics = Column(String(255))
-    DirectX = Column(String(255))
-    size = Column(String(255))
-    Notes = Column(String(255))

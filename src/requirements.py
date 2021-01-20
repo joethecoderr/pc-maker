7eef40c44355a09e5b51b3828c61476c309edd67
Jesus Noel Valdibiezo Mora <jesusnoel.valdibiezomora@outlook.com>
2021-01-19 18:06:32 -0600

    modified requirements.py

--- src/requirements.py
+++ src/requirements.py
@@ -4,8 +4,8 @@ from sqlalchemy import Column, String, Integer, Text
 from base import Base
 
 class LowReqSteam(Base):
-    def __init__(self ,Game_Name, Descr, OS, Processor, Ram, Graphics, DirectX,size,Notes,table_name):
-        self.tablename = tablename
+    def __init__(self ,Game_Name, Descr, OS, Processor, Ram, Graphics, DirectX,size,Notes):
+      
         self.Game_Name = Game_Name
         self.Descr = Descr
         self.OS = OS
@@ -15,20 +15,21 @@ class LowReqSteam(Base):
         self.DirectX = DirectX
         self.size = size
         self.Notes = Notes
-        table()
         
-    def table(self):
-        __tablename__ = self.tablename
-        id = Column(Integer(), primary_key=True, autoincrement = True)
-        Game_Name = Column(String(255))
-        Descr = Column(String(255))
-        OS = Column(String(255))
-        Processor = Column(String(255))
-        Ram = Column(String(255))
-        Graphics = Column(String(255))
-        DirectX = Column(String(255))
-        size = Column(String(255))
-        Notes = Column(String(255))
+        
+    __tablename__ = 'low_req_steam'
+  
+
+    id = Column(Integer(), primary_key=True, autoincrement = True)
+    Game_Name = Column(String(255))
+    Descr = Column(String(255))
+    OS = Column(String(255))
+    Processor = Column(String(255))
+    Ram = Column(String(255))
+    Graphics = Column(String(255))
+    DirectX = Column(String(255))
+    size = Column(String(255))
+    Notes = Column(String(255))
     
 
 class RecReqSteam(Base):
